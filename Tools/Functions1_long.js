a6440e3b52c1b6c4298d91cb862a4b31bb689580
iddo gefen <iddo.gefen@gmail.com>
2025-08-18 01:37:56 -0400

    8.17.25

--- Tools/Functions1_long.js
+++ Tools/Functions1_long.js
@@ -157,7 +157,7 @@ function getTimeline(seq,p1,p2,choices,num,part,slow) {
 	 	duration_title			= 6000;
 	 	duration_image			= 4500;
 	 	duration_question		= 4000;
-	 	duration_response		= 5400;
+	 	duration_response		= 10800;
 	}
 
 	// create a "trial_10" timeline
